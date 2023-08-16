a85b8b340e5915259e70e790f3ca6698db0035f8
quypro <s2boybuons2@gmail.com>
2023-08-16 16:27:58 +0700

    Update

--- update.bat
+++
@@ -1,1 +0,0 @@
-bash -c "cd C:\Users\Admin\Documents\GitHub\quypro; ./update.sh;"
--- update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-bzip2 -c9k ./Packages > ./Packages.bz2
-printf "Origin: QUYIOS\nLabel: QUYIOS\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitecture: iphoneos-arm\nComponents: main\nDescription: QUYIOS\nMD5Sum:\n "$(cat ./Packages | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages)" Packages\n "$(cat ./Packages.bz2 | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages.bz2)" Packages.bz2\n" >Release;
-exit 0
